8f1cea05e3f8f4182574eb6717f16c4613b4d6c2
Mont-Huet <monse77cruz@gmail.com>
2024-08-08 16:18:47 -0600

    Agregamos información en el archivo README

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-//console.log("Hola Mundo! desde la consola");
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
     loop: true
 });
 
-typewriter.typeString("Ing. en sistemas computacionales|Desarrolladora Frontend Jr")
+typewriter.typeString('Ing. En sistemas computacionales | Desarrolladora Frontend Jr.')
     .pauseFor(2500)
     .deleteAll()
+    .typeString('¡Si lo puedes imaginar, lo puedes programar!')
+    .pauseFor(2500)
+    .deleteChars(6)
+    .typeString('')
+    .pauseFor(2500)
     .start();
